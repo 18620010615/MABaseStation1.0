526e8ba7ae6b7a62966e545b8efbf9943bc60aec
loop <loop@loopdeMac.local>
2018-04-28 15:42:16 +0800

    Initial Commit

---
+++ MABaseStation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MABaseStation
+//
+//  Created by loop on 2018/4/26.
+//  Copyright © 2018年 loop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
++ (UIImage *)imageWithColor:(UIColor *)color;
+
+@end
+
